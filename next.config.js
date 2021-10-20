e6058bd14f3fe9eacc39cfde70f86d2c4178e23e
Carlos Rivera <me@carlosrivera.im>
2021-10-20 03:34:02 -0500

    updated next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        PUBLIC_URL: "https://synx.ai",
+        assetPrefix: './'
+    },
+    images: {
+        loader: "imgix",
+        path: "https://synx.ai",
+    }
+};
